51d97d1c8df1c9ec6290cb35ed2d1cb051b7e925
unknown <minasoltan@windowslive.com>
2021-04-26 22:24:23 +0200

    adding particles.js and Rank styling

--- facerecognotionbrain/src/components/ImageLinkForm/ImageLinkForm.js
+++ facerecognotionbrain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
                 {"This Magic Brain Will Detect Faces In Your Pictures. Give it a Try"}
             </p>
             <div className="center ImageLinkForm">
-                <input type="text" className="f4 pa2 w-70 center" />
+                <input type="text" className="f4 pa2 w-70 center urlHolder" />
                 <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
             </div>
         </div>
---
+++ facerecognotionbrain/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3 center">
+                {"Mina, Your Current Rank is "}
+            </div>
+            <div className="white f1 center">
+                {"#5"}
+            </div>
+        </div>
+    )
+}
+export default Rank;
